7e4a16a73f3172d8f6d08d1976cda38dca3b7bbf
jigsaw <m@jgs.me>
2024-02-10 23:32:36 +0000

    impl

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import { Database } from "bun:sqlite";
+import { Hono } from "hono";
+
+const db = new Database(Bun.env.DB_PATH, {
+  readonly: true,
+});
+
+const app = new Hono();
+app.get("/", (c) => c.text("It works!"));
+app.get("/snapshots", (c) => {
+  const { page } = c.req.query();
+  const parsedPage = page ? parseInt(page, 10) * 20 : 0;
+  const query = db.query(
+    `select id, title, url from core_snapshot order by added desc limit 20 offset ${parsedPage}`
+  );
+  return c.json(query.all());
+});
+
+export default app;
